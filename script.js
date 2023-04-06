9ba72b9727d724de26eb22d643cfd9bd95e4dfee
Ranji-moon <sararangel401@gmail.com>
2023-04-05 22:09:10 -0300

    Iniciando com JS

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('Olá mundo');
+//prompt('Qual é a sua idade?');
+
+
+//conhecendo as variaveis//
+const dataHoje = 05;
+let texto = 'A data de hoje é:';
+console.log(texto)
+console.log(dataHoje)
+
+//concatenando variaveis
+console.log(texto + dataHoje)
+
+//criando variaveis- exemplos
+const nomeFirst = 'Sara'
+const nomeLast = 'Rangel'
+let minhaIdade = 17
+let souEstudante = true
+
+//Imprimir no console
+console.log('Meu nome é', nomeFirst, nomeLast, '.Tenho', minhaIdade, 'e sou estudante:', souEstudante)
+
